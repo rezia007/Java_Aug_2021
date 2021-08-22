bcc2d125754c95dbc9c53aacb5bce632565f63cc
Rezia <rezia.parvin007@gmail.com>
2021-08-22 13:26:02 -0400

    3rd Commit

---
+++ src/collectionsframework/MapInterface.java
@@ -0,0 +1,24 @@
+package collectionsframework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapInterface {
+    public static void main(String args[])
+    {
+        Map<String, Integer> hm
+                = new HashMap<String, Integer>();
+
+        hm.put("a", new Integer(100));
+        hm.put("b", new Integer(200));
+        hm.put("c", new Integer(300));
+        hm.put("d", new Integer(400));
+
+        // Traversing through the map
+        for (Map.Entry<String, Integer> me : hm.entrySet()) {
+            System.out.print(me.getKey() + ":");
+            System.out.println(me.getValue());
+        }
+    }
+
+}
---
+++ src/collectionsframework/SetInterface.java
@@ -0,0 +1,19 @@
+package collectionsframework;
+import java.util.*;
+
+public class SetInterface {
+
+        public static void main(String[] args)
+        {
+            Set<String> hash_Set = new HashSet<String>();
+
+            hash_Set.add("Java");
+            hash_Set.add("is");
+            hash_Set.add("easy");
+            hash_Set.add("for");
+            hash_Set.add("me");
+
+            System.out.println(hash_Set);
+        }
+
+    }
